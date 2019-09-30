0028d35f1621f0cb101208de0bd848290db7847d
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-30 04:44:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  {:railroads => {:rent_in_dollars => {:pieces => 4} :green => {}}}}
+  {:railroads => {:rent_in_dollars => {:pieces => 4} :green => []}}
 end
 
 def monopoly_with_fourth_tier
